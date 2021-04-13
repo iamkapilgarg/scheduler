3936d987dbfa28cc95de00ccd7978bdc84646dfa
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-04-12 17:51:31 -0700

    DayListItem tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,13 +7,25 @@ export default function DayListItem(props) {
 
   const dayListItemClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots===0
+    "day-list__item--full": props.spots === 0
   });
+
+
+  const getSpotText = () => {
+    if (props.spots === 0) {
+      return 'no spots remaining';
+    } 
+    if (props.spots === 1) {
+      return '1 spot remaining';
+    } 
+    return  props.spots+' spots remaining';
+    
+  }
 
   return (
     <li onClick={() => props.setDay(props.name)} className={dayListItemClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{getSpotText()}</h3>
     </li>
   );
 }
