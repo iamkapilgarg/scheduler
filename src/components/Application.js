d038e3fe55c925774a1a0122d2ead1a8c34a2009
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-04-14 19:31:20 -0700

    adds state to application

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,8 +8,14 @@ import axios from "axios";
 
 export default function Application(props) {
   
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({...prev, days}));
 
   useEffect(() => {
     axios.get('/api/days')
@@ -95,8 +101,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
